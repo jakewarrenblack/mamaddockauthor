6a848ce89e164e55755bb2da34fa465c97b504d5
= <jakewarrenblack01@gmail.com>
2022-02-26 17:40:54 +0000

    Restyle nav on mobile, now opens curtain menu onclick - refinement needed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,18 +3,20 @@
     <div id="container">
       <nav>
         <h2 @click="home()">MA MADDOCK</h2>
-        <ul>
-          <a
-            @click="checkRouter(link)"
-            :href="link.href"
-            v-for="link in this.links"
-            :key="link"
-            class="anchor"
-          >
-            <li>{{ link.title }}</li>
-          </a>
-        </ul>
-        <!-- <li v-for="link in props.links" :key="link">test</li> -->
+        <div id="curtain_contain" class="curtain_contain">
+          <ul id="linklist">
+            <a
+              @click="checkRouter(link)"
+              :href="link.href"
+              v-for="link in this.links"
+              :key="link"
+              class="anchor"
+            >
+              <li>{{ link.title }}</li>
+            </a>
+          </ul>
+        </div>
+        <button @click="openCurtain()" class="nav_toggle">X</button>
       </nav>
     </div>
   </header>
@@ -27,7 +29,9 @@ export default {
     links: Array,
   },
   data() {
-    return {};
+    return {
+      clicks: 0,
+    };
   },
   methods: {
     home() {
@@ -36,10 +40,21 @@ export default {
     checkRouter(link) {
       link.router ? this.router.push(link.href) : "";
     },
+    openCurtain() {
+      this.clicks++;
+
+      const curtain_contain = document.getElementById("curtain_contain");
+
+      if (this.clicks % 2 != 0) {
+        curtain_contain.classList.add("curtainVisible");
+        document.getElementById("header").style.zIndex = "-1";
+      } else {
+        curtain_contain.classList.remove("curtainVisible");
+        document.getElementById("header").style.zIndex = "1";
+      }
+    },
   },
-  mounted() {
-    console.log(this.props.links);
-  },
+  mounted() {},
 };
 </script>
 
@@ -56,6 +71,24 @@ header {
   margin: auto;
   background: transparent;
 }
+
+.curtainVisible {
+  display: block !important;
+  position: absolute;
+  top: 0l;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background: var(--background_red);
+  transform: translateY(0%) !important;
+}
+
+.zIndexMinus1 {
+  z-index: -1 !important;
+}
+
 nav {
   display: flex;
 }
@@ -110,7 +143,7 @@ h2:hover {
     /* flex-direction: column; */
     margin-left: 0;
     padding-left: 0;
-    border-bottom: 2px solid var(--white);
+    /* border-bottom: 2px solid var(--white); */
   }
 
   header {
@@ -130,4 +163,59 @@ h2:hover {
     padding-left: 0;
   }
 }
+
+/* ------------------------------------------------- */
+
+.curtain_contain {
+  display: flex;
+  width: 100%;
+}
+
+.nav_toggle {
+  display: none;
+  position: static;
+  width: 10%;
+  margin-right: auto;
+  z-index: 99999999999999999999999999999999999999999999;
+}
+
+@media only screen and (max-width: 767px) {
+  li {
+    margin: 1rem;
+  }
+
+  #linklist {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30vh;
+  }
+
+  .curtain_contain {
+    /* display: none; */
+
+    width: 100vw;
+    position: absolute;
+    z-index: 99999999999999999;
+    height: 100vh;
+    /* top: 15%; */
+    transform: translateY(-100%);
+    transition: all ease-in-out 0.2s;
+  }
+
+  nav {
+    flex-direction: row;
+    align-items: center;
+    margin-left: auto;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    width: 75%;
+  }
+  .nav_toggle {
+    display: block;
+  }
+}
+
+/* ------------------------------------------------- */
 </style>
